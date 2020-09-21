555bc08b475c521c72855fb1a9c4d5ec0c3dce87
zlee1 <zack.lee@valpo.edu>
2020-09-21 15:05:30 -0400

    Added comments

--- stackgame.py
+++ stackgame.py
@@ -1,71 +1,95 @@
+# Draw the board.
 def draw(p1, p2, p3):
+    # This is the total number of discs.
     max = len(p1) + len(p2) + len(p3) - 1
 
     print()
     for i in range(max+1):
         for p in [p1, p2, p3]:
+            # Print the pillar item at this index. Otherwise print pipe to
+            # represent a segment of the pillar.
             try:
                 print(p[max-i], end="\t")
             except:
                 print("|", end="\t")
         print()
 
+# Initialize the board.
 def initBoard(max):
+    # Each array represents a pillar of the board.
     p1 = []
     p2 = []
     p3 = []
 
+    #Populate the first pillar with numbers to represent disc size.
     for i in range(max):
         p1.append(max-i)
 
     return p1, p2, p3
 
+# Check to see if the move the user attempted to make is valid.
 def checkValidMove(p1, p2, p3, a, b):
     pillars = [p1, p2, p3]
 
+    # If the user tried to move a disc to the pillar it is currently on or the user
+    # tried to move from an empty pillar, the move is invalid.
     if a == b or len(pillars[a]) == 0:
         return False
+    # If the user tried to move to an empty pillar, the move is valid.
     elif len(pillars[b]) == 0:
         return True
+    # If the user tried to move a disc onto a smaller disc, the move is invalid.
     elif pillars[a][len(pillars[a])-1] > pillars[b][len(pillars[b])-1]:
         return False
     else:
         return True
 
+# Execute the user's move on the pillars.
 def makeMove(p1, p2, p3, a, b):
     pillars = [p1, p2, p3]
+    # Remove the disc at the top of Pillar A and put it on Pillar B.
     pillars[b].append(pillars[a].pop())
     return p1, p2, p3
 
+# Allow the user to make a move.
 def turn(p1, p2, p3):
     success = False
+
+    # Repeat as long as a valid move has not been made.
     while(not success):
         try:
             a = int(input("Move from pillar (1-3): ")) - 1
             b = int(input("To pillar (1-3): ")) - 1
+
+            # If a valid move has been selected, make the move and end the loop.
+            if checkValidMove(p1, p2, p3, a, b):
+                success = True
+                p1, p2, p3 = makeMove(p1, p2, p3, a, b)
+            else:
+                print("Invalid move.")
         except:
             pass
-        if checkValidMove(p1, p2, p3, a, b):
-            success = True
-            p1, p2, p3 = makeMove(p1, p2, p3, a, b)
-        else:
-            print("Invalid move.")
     return p1, p2, p3
 
+# Check to see if the user has won.
 def checkWin(p1, p2, p3):
+    # If all of the discs have been moved to another pillar, the user has won.
     if (len(p1) == 0 and len(p3) == 0) or (len(p1) == 0 and len(p2) == 0):
         return True
     else:
         return False
 
+# Run the game
 def run(n):
     turns = 0
-
     p1, p2, p3 = initBoard(n)
+
+    # Continue playing until the user wins.
     while not checkWin(p1, p2, p3):
         draw(p1, p2, p3)
         p1, p2, p3 = turn(p1, p2, p3)
         turns += 1
+
     draw(p1, p2, p3)
     print("You won in {} turns!".format(turns))
 
